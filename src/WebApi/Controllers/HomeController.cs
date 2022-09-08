3967ff40091ab029bb68a2b5b63b5e1d8ed220fb
m.holtsev <DoctorMatt47@gmail.com>
2022-09-08 21:50:19 +0300

    Added home route redirect to swagger

---
+++ src/WebApi/Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GroupProject.WebApi.Controllers;
+
+[Route("/")]
+[ApiExplorerSettings(IgnoreApi = true)]
+public class HomeController : ControllerBase
+{
+    public ActionResult RedirectToSwagger() => Redirect("/swagger/index.html");
+}
