46b63ec6b931d4f798807b8930c6da2b31ae5cb9
m.holtsev <DoctorMatt47@gmail.com>
2022-10-07 02:24:13 +0300

    Implemented database context
    Signed-off-by: m.holtsev <DoctorMatt47@gmail.com>

---
+++ src/Infrastructure/Persistence/Contexts/AppContext.cs
@@ -0,0 +1,28 @@
+﻿using GroupProject.Application.Common.Interfaces;
+using GroupProject.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GroupProject.Infrastructure.Persistence.Contexts;
+
+public sealed class AppContext : DbContext, IAppContext
+{
+    public AppContext(DbContextOptions options) : base(options)
+    {
+        Database.EnsureCreated();
+    }
+
+    public DbSet<User> Users { get; set; } = null!;
+    public DbSet<Topic> Topics { get; set; } = null!;
+    public DbSet<Complaint> Complaints { get; set; } = null!;
+    public DbSet<Commentary> Commentaries { get; set; } = null!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
