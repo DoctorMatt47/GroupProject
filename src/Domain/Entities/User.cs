1b4da8a760c99748554f99a651def9f33781bb2b
m.holtsev <DoctorMatt47@gmail.com>
2022-10-07 01:00:27 +0300

    Added user entity

---
+++ src/Domain/Entities/User.cs
@@ -0,0 +1,38 @@
+﻿using GroupProject.Domain.Enums;
+using GroupProject.Domain.Interfaces;
+
+// ReSharper disable CollectionNeverUpdated.Local
+
+namespace GroupProject.Domain.Entities;
+
+public class User
+{
+    private readonly List<Commentary> _commentaries = new();
+    private readonly List<Topic> _questions = new();
+
+    /// <summary>
+    ///     Parameterless constructor, intended only for orm usage.
+    /// </summary>
+    protected User()
+    {
+    }
+
+    public User(string login, string password, IPasswordHashService passwordHash, UserRole role)
+    {
+        Id = Guid.NewGuid();
+        Login = login;
+        PasswordSalt = passwordHash.GenerateSalt();
+        PasswordHash = passwordHash.Encode(password, PasswordSalt);
+        Role = role;
+    }
+
+    public Guid Id { get; protected set; }
+    public string Login { get; protected set; } = null!;
+    public byte[] PasswordHash { get; protected set; } = null!;
+    public byte[] PasswordSalt { get; protected set; } = null!;
+    public UserRole Role { get; set; } = UserRole.User;
+    public UserStatus Status { get; set; } = UserStatus.Active;
+
+    public IEnumerable<Topic> Questions => _questions.ToList();
+    public IEnumerable<Commentary> Commentaries => _commentaries.ToList();
+}
---
+++ src/Domain/Enums/UserRole.cs
@@ -0,0 +1,8 @@
+﻿namespace GroupProject.Domain.Enums;
+
+public enum UserRole
+{
+    User,
+    Moderator,
+    Admin,
+}
