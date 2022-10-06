8fa3c9cd03e30afa4576f4f4b24a228197ba543c
m.holtsev <DoctorMatt47@gmail.com>
2022-10-07 01:02:34 +0300

    Implemented password hash service

---
+++ src/Application/Common/Services/PasswordHashService.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Cryptography;
+using GroupProject.Domain.Interfaces;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+
+namespace GroupProject.Application.Common.Services;
+
+public class PasswordHashService : IPasswordHashService
+{
+    private const int BitCount = 8;
+    private const int IterationCount = 100_000;
+    private const int SubkeyBitCount = 256;
+
+    public byte[] GenerateSalt()
+    {
+        var salt = new byte[16];
+        RandomNumberGenerator.Create().GetBytes(salt);
+        return salt;
+    }
+
+    public byte[] Encode(string password, byte[] passwordSalt) =>
+        KeyDerivation.Pbkdf2(
+            password,
+            passwordSalt,
+            KeyDerivationPrf.HMACSHA256,
+            IterationCount,
+            SubkeyBitCount / BitCount);
+}
---
+++ src/Domain/Interfaces/IPasswordHashService.cs
@@ -0,0 +1,7 @@
+﻿namespace GroupProject.Domain.Interfaces;
+
+public interface IPasswordHashService
+{
+    byte[] GenerateSalt();
+    byte[] Encode(string password, byte[] salt);
+}
