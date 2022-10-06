9726ec6ce3fba2b7e3a54e3563a71eac9752f695
m.holtsev <DoctorMatt47@gmail.com>
2022-10-07 01:00:53 +0300

    Added topic entity

---
+++ src/Domain/Entities/Topic.cs
@@ -0,0 +1,35 @@
+﻿namespace GroupProject.Domain.Entities;
+
+public class Topic
+{
+    // ReSharper disable once CollectionNeverUpdated.Local
+    private readonly List<Complaint> _complaints = new();
+
+    /// <summary>
+    ///     Parameterless constructor, intended only for orm usage.
+    /// </summary>
+    protected Topic()
+    {
+    }
+
+    public Topic(string header, string description, string? code, Guid userId)
+    {
+        Id = Guid.NewGuid();
+        CreationTime = DateTime.UtcNow;
+        UserId = userId;
+        Header = header;
+        Description = description;
+        Code = code;
+    }
+
+    public Guid Id { get; protected set; }
+    public DateTime CreationTime { get; protected set; }
+    public string Header { get; protected set; } = null!;
+    public string Description { get; protected set; } = null!;
+    public string? Code { get; protected set; }
+
+    public Guid UserId { get; protected set; }
+    public User User { get; protected set; } = null!;
+
+    public IEnumerable<Complaint> Complaints => _complaints.ToList();
+}
