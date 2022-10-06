8394b731997b00a546dcffa547289d31048b10cb
m.holtsev <DoctorMatt47@gmail.com>
2022-10-07 01:02:04 +0300

    Added complaint entity

---
+++ src/Domain/Entities/Complaint.cs
@@ -0,0 +1,26 @@
+﻿namespace GroupProject.Domain.Entities;
+
+public class Complaint
+{
+    /// <summary>
+    ///     Parameterless constructor, intended only for orm usage.
+    /// </summary>
+    protected Complaint()
+    {
+    }
+
+    public Complaint(string description, Guid questionId)
+    {
+        Id = Guid.NewGuid();
+        CreationTime = DateTime.UtcNow;
+        Description = description;
+        QuestionId = questionId;
+    }
+
+    public Guid Id { get; protected set; }
+    public DateTime CreationTime { get; protected set; }
+    public string Description { get; protected set; } = null!;
+
+    public Guid QuestionId { get; protected set; }
+    public Topic Topic { get; protected set; } = null!;
+}
