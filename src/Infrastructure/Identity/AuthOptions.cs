d92bfd56fb9539c054a5d1ac2533c6c945875b76
m.holtsev <DoctorMatt47@gmail.com>
2022-10-07 19:57:18 +0300

    Implemented auth options
    Signed-off-by: m.holtsev <DoctorMatt47@gmail.com>

---
+++ src/Infrastructure/Identity/AuthOptions.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+using GroupProject.Application.Identity;
+using Microsoft.IdentityModel.Tokens;
+
+namespace GroupProject.Infrastructure.Identity;
+
+public class AuthOptions : IAuthOptions
+{
+    private const string Key = "GroupProject-SecretKey";
+
+    public string Issuer => "GroupProject-SecretKey";
+    public string Audience => "GroupProject-SecretKey";
+
+    public SymmetricSecurityKey GetSymmetricSecurityKey() => new(Encoding.ASCII.GetBytes(Key));
+}
