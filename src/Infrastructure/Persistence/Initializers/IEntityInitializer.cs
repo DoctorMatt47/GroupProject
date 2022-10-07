c27fbf5e1885516dc79971ce81efecdd31c34f43
m.holtsev <DoctorMatt47@gmail.com>
2022-10-07 19:58:59 +0300

    Added user entity initializer
    Signed-off-by: m.holtsev <DoctorMatt47@gmail.com>

---
+++ src/Infrastructure/Persistence/Initializers/IEntityInitializer.cs
@@ -0,0 +1,6 @@
+﻿namespace GroupProject.Infrastructure.Persistence.Initializers;
+
+public interface IEntityInitializer
+{
+    public void Initialize();
+}
---
+++ src/Infrastructure/Persistence/Initializers/UserInitializer.cs
@@ -0,0 +1,31 @@
+﻿using GroupProject.Application.Common.Interfaces;
+using GroupProject.Domain.Entities;
+using GroupProject.Domain.Enums;
+using GroupProject.Domain.Interfaces;
+
+namespace GroupProject.Infrastructure.Persistence.Initializers;
+
+public class UserInitializer : IEntityInitializer
+{
+    private readonly IAppDbContext _context;
+    private readonly IPasswordHashService _passwordHash;
+
+    public UserInitializer(IAppDbContext context, IPasswordHashService passwordHash)
+    {
+        _context = context;
+        _passwordHash = passwordHash;
+    }
+
+    public void Initialize()
+    {
+        var user = new User("user", "password", _passwordHash, UserRole.User);
+        var moderator = new User("moderator", "password", _passwordHash, UserRole.Moderator);
+        var admin = new User("admin", "password", _passwordHash, UserRole.Admin);
+
+        _context.Set<User>().Add(user);
+        _context.Set<User>().Add(moderator);
+        _context.Set<User>().Add(admin);
+
+        _context.SaveChangesAsync().GetAwaiter().GetResult();
+    }
+}
