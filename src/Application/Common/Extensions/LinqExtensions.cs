cb366eb33b5ca343f9a706154a2c9bc97f776bd9
m.holtsev <DoctorMatt47@gmail.com>
2022-10-07 18:07:45 +0300

    Added linq extensions
    Signed-off-by: m.holtsev <DoctorMatt47@gmail.com>

---
+++ src/Application/Common/Extensions/LinqExtensions.cs
@@ -0,0 +1,22 @@
+﻿using GroupProject.Application.Common.Responses;
+using Microsoft.EntityFrameworkCore;
+
+namespace GroupProject.Application.Common.Extensions;
+
+public static class LinqExtensions
+{
+    public static async Task<Page<T>> ToPageAsync<T>(this IQueryable<T> queryable, int pageCount) =>
+        new(await queryable.ToListAsync(), pageCount);
+
+    public static async Task<IEnumerable<TResponse>> WhenAllAsync<TResponse>(this IEnumerable<Task<TResponse>> tasks)
+    {
+        var responses = new List<TResponse>();
+        foreach (var task in tasks)
+        {
+            var response = await task;
+            responses.Add(response);
+        }
+
+        return responses;
+    }
+}
