0f1bf181f983a523fb7d7c19dee76b24f4db191c
m.holtsev <DoctorMatt47@gmail.com>
2022-10-07 20:00:00 +0300

    Registered infrastructure services
    Signed-off-by: m.holtsev <DoctorMatt47@gmail.com>

---
+++ src/Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using GroupProject.Application.Common.Interfaces;
+using GroupProject.Application.Identity;
+using GroupProject.Infrastructure.Identity;
+using GroupProject.Infrastructure.Persistence.Contexts;
+using GroupProject.Infrastructure.Persistence.Initializers;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GroupProject.Infrastructure.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, string connectionString)
+    {
+        services.AddDbContext<IAppDbContext, AppDbContext>(opts => opts.UseSqlite(connectionString));
+
+        services.AddScoped<IEntityInitializer, UserInitializer>();
+        services.AddSingleton<IAuthOptions, AuthOptions>();
+
+        return services;
+    }
+}
