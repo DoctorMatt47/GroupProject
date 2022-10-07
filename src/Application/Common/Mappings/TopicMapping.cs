8895c0ae868fb3642093e9ca039fd4c64394c74b
m.holtsev <DoctorMatt47@gmail.com>
2022-10-07 18:05:29 +0300

    Added automapper profiles
    Signed-off-by: m.holtsev <DoctorMatt47@gmail.com>

---
+++ src/Application/Common/Mappings/TopicMapping.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using GroupProject.Application.Topics;
+using GroupProject.Domain.Entities;
+
+namespace GroupProject.Application.Common.Mappings;
+
+public class TopicMapping : Profile
+{
+    protected TopicMapping()
+    {
+        CreateMap<Topic, TopicResponse>();
+        CreateMap<Topic, TopicInfoResponse>();
+        CreateMap<Topic, TopicByUserIdResponse>();
+    }
+}
---
+++ src/Application/Common/Mappings/UserMapping.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using GroupProject.Application.Users;
+using GroupProject.Domain.Entities;
+
+namespace GroupProject.Application.Common.Mappings;
+
+public class UserMapping : Profile
+{
+    public UserMapping()
+    {
+        CreateMap<User, UserResponse>();
+    }
+}
