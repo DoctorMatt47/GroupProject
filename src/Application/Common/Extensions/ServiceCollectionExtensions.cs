71d67524c075cce25ce4d870c0a09734055a33b2
m.holtsev <DoctorMatt47@gmail.com>
2022-10-07 19:57:04 +0300

    Registered application services
    Signed-off-by: m.holtsev <DoctorMatt47@gmail.com>

---
+++ src/Application/Common/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using GroupProject.Application.Commentaries;
+using GroupProject.Application.Common.Services;
+using GroupProject.Application.Identity;
+using GroupProject.Application.Topics;
+using GroupProject.Application.Users;
+using GroupProject.Domain.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GroupProject.Application.Common.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        services.AddTransient<IPasswordHashService, PasswordHashService>();
+        services.AddTransient<IJwtTokenService, JwtTokenService>();
+        services.AddTransient<IUserService, UserService>();
+        services.AddTransient<ICommentaryService, CommentaryService>();
+        services.AddTransient<ITopicService, TopicService>();
+
+        return services;
+    }
+}
