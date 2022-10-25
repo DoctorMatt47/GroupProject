3c2ed11a910cd4323400a47e01b42c357ea1fd97
m.holtsev <DoctorMatt47@gmail.com>
2022-10-25 22:37:26 +0300

    Added section entity
    Signed-off-by: m.holtsev <DoctorMatt47@gmail.com>

---
+++ src/Domain/Entities/Section.cs
@@ -0,0 +1,26 @@
+﻿namespace GroupProject.Domain.Entities;
+
+public class Section
+{
+    // ReSharper disable once CollectionNeverUpdated.Local
+    private readonly List<Topic> _topics = new();
+
+    /// <summary>
+    ///     Parameterless constructor, intended only for orm usage.
+    /// </summary>
+    protected Section()
+    {
+    }
+
+    public Section(string header, string description)
+    {
+        Header = header;
+        Description = description;
+    }
+
+    public int Id { get; protected set; }
+    public string Header { get; protected set; } = null!;
+    public string Description { get; protected set; } = null!;
+
+    public IEnumerable<Topic> Topics => _topics.ToList();
+}
