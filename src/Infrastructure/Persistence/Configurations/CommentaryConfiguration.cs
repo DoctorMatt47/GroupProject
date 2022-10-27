0f623a54a6e51626f98b4d28ec13b39f4f6eb735
m.holtsev <DoctorMatt47@gmail.com>
2022-10-27 03:08:31 +0300

    Updated entity configurations
    Signed-off-by: m.holtsev <DoctorMatt47@gmail.com>

--- src/Infrastructure/Persistence/Configurations/CommentaryConfiguration.cs
+++ src/Infrastructure/Persistence/Configurations/CommentaryConfiguration.cs
@@ -9,5 +9,7 @@ public class CommentaryConfiguration : IEntityTypeConfiguration<Commentary>
     public void Configure(EntityTypeBuilder<Commentary> builder)
     {
         builder.HasOne(c => c.User).WithMany(u => u.Commentaries);
+        builder.HasOne(c => c.Topic).WithMany(t => t.Commentaries);
+        builder.HasMany(c => c.Complaints).WithOne(c => c.Commentary);
     }
 }
--- src/Infrastructure/Persistence/Configurations/ComplaintConfiguration.cs
+++ src/Infrastructure/Persistence/Configurations/ComplaintConfiguration.cs
@@ -9,5 +9,6 @@ public class ComplaintConfiguration : IEntityTypeConfiguration<Complaint>
     public void Configure(EntityTypeBuilder<Complaint> builder)
     {
         builder.HasOne(c => c.Topic).WithMany(t => t.Complaints);
+        builder.HasOne(c => c.Commentary).WithMany(t => t.Complaints);
     }
 }
