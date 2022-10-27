9c421d2e094114e7b00a84f01b8615b1ee306983
m.holtsev <DoctorMatt47@gmail.com>
2022-10-27 03:07:18 +0300

    Refactored complaint entity
    Signed-off-by: m.holtsev <DoctorMatt47@gmail.com>

--- src/Domain/Entities/Complaint.cs
+++ src/Domain/Entities/Complaint.cs
@@ -1,4 +1,6 @@
-﻿namespace GroupProject.Domain.Entities;
+﻿using GroupProject.Domain.Enums;
+
+namespace GroupProject.Domain.Entities;
 
 public class Complaint
 {
@@ -9,18 +11,29 @@ public class Complaint
     {
     }
 
-    public Complaint(string description, Guid topicId)
+    public Complaint(string description, ComplaintTarget target, Guid elementId)
     {
         Id = Guid.NewGuid();
         CreationTime = DateTime.UtcNow;
         Description = description;
-        TopicId = topicId;
+        Target = target;
+
+        _ = target switch
+        {
+            ComplaintTarget.Topic => TopicId = elementId,
+            ComplaintTarget.Commentary => CommentaryId = elementId,
+            _ => throw new ArgumentOutOfRangeException(nameof(target), target, null),
+        };
     }
 
     public Guid Id { get; protected set; }
     public DateTime CreationTime { get; protected set; }
     public string Description { get; protected set; } = null!;
+    public ComplaintTarget Target { get; protected set; }
 
-    public Guid TopicId { get; protected set; }
-    public Topic Topic { get; protected set; } = null!;
+    public Guid? TopicId { get; protected set; }
+    public Topic? Topic { get; protected set; }
+
+    public Guid? CommentaryId { get; protected set; }
+    public Commentary? Commentary { get; protected set; }
 }
