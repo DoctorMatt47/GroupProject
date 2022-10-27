305d35b02e8f857ad41beacbb8a4770463e15cbf
m.holtsev <DoctorMatt47@gmail.com>
2022-10-27 03:09:09 +0300

    Added get complaint by commentary id method
    Signed-off-by: m.holtsev <DoctorMatt47@gmail.com>

--- src/Application/Complaints/ComplaintService.cs
+++ src/Application/Complaints/ComplaintService.cs
@@ -25,10 +25,22 @@ public class ComplaintService : IComplaintService
         _mapper = mapper;
     }
 
-    public async Task<IEnumerable<ComplaintResponse>> GetByTopicId(Guid topicId, CancellationToken cancellationToken)
+    public async Task<IEnumerable<ComplaintResponse>> GetByTopicId(
+        Guid topicId,
+        CancellationToken cancellationToken)
     {
         return await _dbContext.Set<Complaint>()
-            .Where(c => c.Id == topicId)
+            .Where(c => c.TopicId == topicId)
+            .ProjectTo<ComplaintResponse>(_mapper.ConfigurationProvider)
+            .ToListAsync(cancellationToken);
+    }
+
+    public async Task<IEnumerable<ComplaintResponse>> GetByCommentaryId(
+        Guid commentaryId,
+        CancellationToken cancellationToken)
+    {
+        return await _dbContext.Set<Complaint>()
+            .Where(c => c.CommentaryId == commentaryId)
             .ProjectTo<ComplaintResponse>(_mapper.ConfigurationProvider)
             .ToListAsync(cancellationToken);
     }
@@ -37,18 +49,19 @@ public class ComplaintService : IComplaintService
         CreateComplaintRequest request,
         CancellationToken cancellationToken)
     {
-        var isTopicExist = await _dbContext.Set<Topic>().AnyAsync(t => t.Id == request.TopicId, cancellationToken);
-        if (!isTopicExist) throw new NotFoundException($"There is no topic with id: {request.TopicId}");
+        var isTopicExist = await _dbContext.Set<Topic>().AnyAsync(t => t.Id == request.ElementId, cancellationToken);
+        if (!isTopicExist) throw new NotFoundException($"There is no topic with id: {request.ElementId}");
 
-        var complaint = new Complaint(request.Description, request.TopicId);
+        var complaint = new Complaint(request.Description, request.Target, request.ElementId);
 
         _dbContext.Set<Complaint>().Add(complaint);
         await _dbContext.SaveChangesAsync(cancellationToken);
 
         _logger.LogInformation(
-            "Created complaint with id: {ComplaintId} on topic: {TopicId}",
+            "Created complaint with id: {ComplaintId} on {Target}: {TopicId}",
             complaint.Id,
-            request.TopicId);
+            Enum.GetName(request.Target),
+            request.ElementId);
 
         return new IdResponse<Guid>(complaint.Id);
     }
--- src/Application/Complaints/IComplaintService.cs
+++ src/Application/Complaints/IComplaintService.cs
@@ -5,5 +5,6 @@ namespace GroupProject.Application.Complaints;
 public interface IComplaintService
 {
     Task<IEnumerable<ComplaintResponse>> GetByTopicId(Guid topicId, CancellationToken cancellationToken);
+    Task<IEnumerable<ComplaintResponse>> GetByCommentaryId(Guid commentaryId, CancellationToken cancellationToken);
     Task<IdResponse<Guid>> CreateComplaint(CreateComplaintRequest request, CancellationToken cancellationToken);
 }
