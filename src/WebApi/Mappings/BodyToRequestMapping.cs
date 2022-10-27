f48de0c9e6d50e04e375ca9b05ef7d085d4484ca
m.holtsev <DoctorMatt47@gmail.com>
2022-10-28 01:35:54 +0300

    Updated complaint body to complaint request mapping
    Signed-off-by: m.holtsev <DoctorMatt47@gmail.com>

--- src/WebApi/Mappings/BodyToRequestMapping.cs
+++ src/WebApi/Mappings/BodyToRequestMapping.cs
@@ -3,6 +3,7 @@ using GroupProject.Application.Commentaries;
 using GroupProject.Application.Common.Extensions;
 using GroupProject.Application.Complaints;
 using GroupProject.Application.Topics;
+using GroupProject.Domain.Enums;
 using GroupProject.WebApi.Requests;
 
 namespace GroupProject.WebApi.Mappings;
@@ -16,6 +17,7 @@ public class BodyToRequestMapping : Profile
             .MapRecordMember(r => r.TopicId, _ => Guid.Empty);
 
         CreateMap<CreateComplaintBody, CreateComplaintRequest>()
+            .MapRecordMember(r => r.Target, _ => ComplaintTarget.Topic)
             .MapRecordMember(r => r.ElementId, _ => Guid.Empty);
 
         CreateMap<CreateTopicBody, CreateTopicRequest>()
