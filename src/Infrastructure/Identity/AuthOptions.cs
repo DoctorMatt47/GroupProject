94a8feee469a4a6544336cfbe83bcf5f8d502746
m.holtsev <DoctorMatt47@gmail.com>
2022-10-30 01:15:53 +0300

    Refactored auth options
    Signed-off-by: m.holtsev <DoctorMatt47@gmail.com>

--- src/Infrastructure/Identity/AuthOptions.cs
+++ src/Infrastructure/Identity/AuthOptions.cs
@@ -1,16 +1,14 @@
-﻿using System.Diagnostics.CodeAnalysis;
-using System.Text;
+﻿using System.Text;
 using Microsoft.IdentityModel.Tokens;
 
 namespace GroupProject.Infrastructure.Identity;
 
-[SuppressMessage("ReSharper", "AutoPropertyCanBeMadeGetOnly.Local")]
 public class AuthOptions
 {
     private const string SecretKey = "GroupProject-SecretKey";
 
-    public string Issuer { get; private set; } = null!;
-    public string Audience { get; private set; } = null!;
+    public string Issuer { get; set; } = null!;
+    public string Audience { get; set; } = null!;
 
     public SymmetricSecurityKey GetSymmetricSecurityKey() => new(Encoding.ASCII.GetBytes(SecretKey));
 }
