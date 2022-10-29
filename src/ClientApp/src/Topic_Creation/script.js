8045932beb5a442f47d227e096551a18c77b8b8f
Megarekrut65 <55782081+Megarekrut65@users.noreply.github.com>
2022-10-29 21:55:04 +0300

    Added some comments

--- src/ClientApp/src/Topic_Creation/script.js
+++ src/ClientApp/src/Topic_Creation/script.js
@@ -1,11 +1,19 @@
+/**
+ * Makes topic creation form visible
+ */
 const openForm = () => {
     document.getElementById("fullscreen-container").style.display = "block";
 };
-
+/**
+ * Makes topic creation form invisible
+ */
 const closeForm = () => {
     document.getElementById("fullscreen-container").style.display = "none";
 };
-
+/**
+ * Adds options (languages) from LANGUAGES to select by id
+ * @param {string} selectId - id of `select` html tag to add to it options
+ */
 const addLanguagesToSelect = (selectId) => {
     let select = document.getElementById(selectId);
     console.log(select)
@@ -16,6 +24,12 @@ const addLanguagesToSelect = (selectId) => {
         select.appendChild(option);
     }
 };
+/**
+ * 
+ * @param {boolean} visible - true to make visible else false
+ * @param {string} id - id of html tag
+ * @param {string} display - type of display if it will be visible
+ */
 const setVisible = (visible, id, display) => {
     document.getElementById(id).style.display = visible?display:'none';
 };
@@ -27,6 +41,10 @@ const needCodeCreate = document.getElementById("need-code");
 const codeCreate = document.getElementById("topic-code");
 const languageCreate = document.getElementById("topic-code-language");
 
+/**
+ * Gets data from from and sends http request to server.
+ * If request is successful than open topic page else gets error
+ */
 const submitTopic = () => {
     createTopic(sectionCreate.value,
         titleCreate.value, 
