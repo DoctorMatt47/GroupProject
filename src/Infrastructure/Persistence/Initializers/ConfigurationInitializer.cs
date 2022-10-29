2209e53a8a8211826dbea3d62fda2984e21592f1
m.holtsev <DoctorMatt47@gmail.com>
2022-10-29 21:26:12 +0300

    Added configuration initializer
    Signed-off-by: m.holtsev <DoctorMatt47@gmail.com>

---
+++ src/Infrastructure/Persistence/Initializers/ConfigurationInitializer.cs
@@ -0,0 +1,25 @@
+﻿using GroupProject.Application.Common.Interfaces;
+using GroupProject.Domain.Entities;
+
+namespace GroupProject.Infrastructure.Persistence.Initializers;
+
+public class ConfigurationInitializer : IEntityInitializer
+{
+    private readonly IAppDbContext _dbContext;
+
+    public ConfigurationInitializer(IAppDbContext dbContext) => _dbContext = dbContext;
+
+    public void Initialize()
+    {
+        if (_dbContext.Set<Configuration>().Any()) return;
+
+        var configuration = (Configuration) Activator.CreateInstance(typeof(Configuration))!;
+
+        configuration.Rules = string.Empty;
+        configuration.BanDuration = TimeSpan.FromMinutes(2);
+        configuration.WarningCountForBan = 2;
+
+        _dbContext.Set<Configuration>().Add(configuration);
+        _dbContext.SaveChangesAsync().GetAwaiter().GetResult();
+    }
+}
