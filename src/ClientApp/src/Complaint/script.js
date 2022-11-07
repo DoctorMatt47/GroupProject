d447632a662d4a9173a91bba47c5cb4fe8c30cc6
Nepochatova Polina <nepochatova.polina@unicyb.kiev.ua>
2022-11-07 20:29:49 +0200

    topic error fixed Topics in section page added

--- src/ClientApp/src/Complaint/script.js
+++ src/ClientApp/src/Complaint/script.js
@@ -4,7 +4,7 @@
  * @param {string} objectId - id of topic or comment
  */
 const openComplainForm = (type, objectId) =>{
-    const container = document.getElementById("fullscreen-container");
+    const container = document.getElementById("complaint-container");
     container.style.display = "block";
     container.onclick = (event)=>{
         if(event.target.id === container.id) closeForm();
@@ -15,13 +15,13 @@ const openComplainForm = (type, objectId) =>{
 };
 
 const closeComplainForm = () => {
-    document.getElementById("fullscreen-container").style.display = "none";
+    document.getElementById("complaint-container").style.display = "none";
 };
 /**
  * Creates complain for topic or comment with description from form
  */
 const submitComplaint = () =>{
-    const container = document.getElementById("fullscreen-container");
+    const container = document.getElementById("complaint-container");
     const type = container.getAttribute("object-type");
     const id = container.getAttribute("object-id");
     const create = type === "topic"?createTopicComplaint:createCommentComplaint;
