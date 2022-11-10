232bf7b7b3d62a6bce7be8027d5dc443e40ab69c
m.holtsev <DoctorMatt47@gmail.com>
2022-11-11 00:11:52 +0200

    Added not implemented hosted service
    Signed-off-by: m.holtsev <DoctorMatt47@gmail.com>

---
+++ src/Application/Common/Services/DeleteExpiredDataService.cs
@@ -0,0 +1,28 @@
+﻿using GroupProject.Application.Common.Interfaces;
+using GroupProject.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace GroupProject.Application.Common.Services;
+
+public class DeleteExpiredDataService : IDeleteExpiredDataService
+{
+    private readonly IAppDbContext _dbContext;
+    private readonly ILogger<DeleteExpiredDataService> _logger;
+
+    public DeleteExpiredDataService(IAppDbContext dbContext, ILogger<DeleteExpiredDataService> logger)
+    {
+        _dbContext = dbContext;
+        _logger = logger;
+    }
+
+    public async Task DeleteExpiredComplaints(CancellationToken cancellationToken)
+    {
+        var complaints = await _dbContext.Set<Complaint>()
+            .Where(c => c.ExpirationTime < DateTime.UtcNow)
+            .ToListAsync(cancellationToken);
+
+        _dbContext.Set<Complaint>().RemoveRange(complaints);
+        await _dbContext.SaveChangesAsync(cancellationToken);
+    }
+}
