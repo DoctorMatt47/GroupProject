6b0a28aaaf9beba8da6a44beaccbcf00fd40e0d9
m.holtsev <DoctorMatt47@gmail.com>
2022-11-17 21:52:58 +0200

    Fixed get users method
    Signed-off-by: m.holtsev <DoctorMatt47@gmail.com>

--- src/WebApi/Controllers/UsersController.cs
+++ src/WebApi/Controllers/UsersController.cs
@@ -13,7 +13,7 @@ public class UsersController : ApiControllerBase
 
     public UsersController(IUserService users) => _users = users;
 
-    [Authorize("Moderator, Admin")]
+    [Authorize(Roles = "Moderator, Admin")]
     [HttpGet]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status401Unauthorized)]
